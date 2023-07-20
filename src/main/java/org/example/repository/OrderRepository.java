25cc4ac4cc985f0faa21ed0cba8eae22413f1559
Saida <58253905+pikozek@users.noreply.github.com>
2023-07-20 17:27:23 +0600

    Delete src/main/java directory

--- src/main/java/org/example/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.dto.Order;
-
-public interface OrderRepository {
-    Order save(Order order);
-
-    void delete(Order order);
-}
--- src/main/java/org/example/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.dto.User;
-
-public interface UserRepository {
-    User save(User user);
-
-    void delete(User user);
-}
